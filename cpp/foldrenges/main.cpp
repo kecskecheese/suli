f4666e00907f2424d01842cbacdf6d99acf7ae66
kecskecheese <96261163+kecskecheese@users.noreply.github.com>
2023-10-10 13:39:57 +0200

    Add files via upload

--- cpp/foldrenges/main.cpp
+++ cpp/foldrenges/main.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-int Mpbe(int o, int p, int mp)
+int Mpbe(int o, int p, int mp int n)
 {
-    return o*3600 + p*60 + mp;
+    return n*86400 + o*3600 + p*60 + mp;
 }
 
 void Mpbol(int mpek)
@@ -19,12 +19,13 @@ void Mpbol(int mpek)
 
     int mp = mpek / 60;
 
-    cout << nap << " " << ora << " " << perc << " " << mp << endl;
+    cout << nap << " " << ora << ":" << perc << ":" << mp << endl;
 }
 
 int main()
 {
     int n;
+    Tldo [1001]
     int nap[1001], ora[1001], perc[1001],masodperc[1001];
 
     //Beolvasas
@@ -34,7 +35,14 @@ int main()
         cin >> nap[i] >> ora[i] >> perc[i] >> masodperc[i];
     }
 
+    // Idõkülönbség számolása
+    int kul[1001];
+    for (int i=1; i<n; i++)
+    {
+        kul[i] = Mpbe(nap[i+1], ora[i+1], perc[i+1], masodperc[i+1])- Mpbe(nap[i], ora[i], perc[i], masodperc[i]);
+    }
 
+    //maxkivalasztas
 
     return 0;
 }
