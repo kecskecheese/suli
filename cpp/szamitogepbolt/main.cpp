4a2eba45d4d0054af5bc55d60d687d3b56a2f5c3
kecskecheese <96261163+kecskecheese@users.noreply.github.com>
2023-11-21 15:17:21 +0100

    Add files via upload

--- cpp/szamitogepbolt/main.cpp
+++ cpp/szamitogepbolt/main.cpp
@@ -41,5 +41,35 @@ int main()
     cout << fixed;
     cout << "Az atlag ar: " << (float)osszeg/n << endl;
 
+    //3. feladat
+    int maxind=ar[1];
+    for(int i=2; i<=n; i++)
+    {
+        if (ar[maxind]<ar[i])
+        {
+            maxind=i;
+        }
+    }
+
+    cout << "A legdragabb termek adatai: " << fajta[maxind] <<" "<<raktar[maxind]<<" "<< ar[maxind] << endl;
+
+    //4. feladat
+    int sokvan=raktar[1];
+    for(int i=1; i<=n; i++)
+    {
+        if (raktar[i]>10)
+            {
+                sokvan=raktar[i];
+            }
+    }
+    if (sokvan>10)
+    {
+        cout << "Van olyan termek amibol 10-nel tobb van" <<endl;
+    }
+    else{
+        cout << "Nincs olyan termek amibol 10-nel tobb van" << endl;
+    }
+
+
     return 0;
 }
